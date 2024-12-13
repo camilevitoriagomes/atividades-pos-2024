29bd611e44cff179180b535b8b892e46c432b2c1
Camile Vitoria Gomes da Silva <camile.vitoria@escolar.ifrn.edu.br>
2024-12-13 12:23:43 -0300

    teste cliente

--- psg/config/urls.py
+++ psg/config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
 from api import views
 
